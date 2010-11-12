cbee4bee3a21d8faf6fe88db8fa4fc68c6d630f2
Pavan <itspanzi@gmail.com>
2010-11-12 16:25:53 +0530

    Added sleepy philosophers. Also ignored some IntelliJ folders.

---
+++ ant_junit/src/com/github/tlb/sampleprojects/SleepyPhilosopher.java
@@ -0,0 +1,17 @@
+package com.github.tlb.sampleprojects;
+
+import org.hamcrest.Matcher;
+
+/**
+ * @understands the concept of a philosopher who needs to sleep in order to come up with her philosophy
+ */
+public class SleepyPhilosopher {
+
+    public void sleepFor(int seconds) {
+        try {
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {
+            //Ignore the demons trying to wake you up
+        }
+    }
+}
