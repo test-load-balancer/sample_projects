b91aae5ff479e63b923be90866498d7787da67b4
Janmejay Singh <singh.janmejay@gmail.com>
2010-11-20 11:21:58 +0530

    Added a  script to bootstrap the test process

---
+++ ant_junit/run_ant.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export CRITERIA_DEFAULTING_ORDER='com.github.tlb.splitter.TimeBasedTestSplitterCriteria:com.github.tlb.splitter.CountBasedTestSplitterCriteria' 
+export TLB_CRITERIA='com.github.tlb.splitter.DefaultingTestSplitterCriteria' 
+export TOTAL_PARTITIONS=2
+export TLB_URL='http://localhost:7019' 
+export TALK_TO_SERVICE='com.github.tlb.service.TalkToTlbServer' 
+export JOB_VERSION=1.2
+export TLB_JOB_NAME='sample-ant' 
+ant test.time_balanced -v
