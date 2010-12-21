4017bd1cf30a9dbc6e982c39f88b4ad408c4a8bc
Janmejay Singh <singh.janmejay@gmail.com>
2010-12-21 21:51:05 +0530

    moved to using tlb gem

--- rspec2_example/run_balanced.sh
+++ rspec2_example/run_balanced.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export TLB_JAR='vendor/tlb_rb/tlb-all-gv0.2-9-g1f1a4aa.jar'
+export TLB_JAR=`ruby -e 'require "rubygems"; require "tlb"; puts Tlb.tlb_jar;'`
 export TLB_JOB_NAME='rspec-2' 
 export TOTAL_PARTITIONS=2
 export TEST_TASK='rake bal'
