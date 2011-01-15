fe2a592fa526653c341dfc6858533823377fd18a
Janmejay Singh <singh.janmejay@gmail.com>
2011-01-15 16:41:54 +0530

    moved it to the recipe style, use run_balanaced.sh

--- ant_junit/run_ant.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export CRITERIA_DEFAULTING_ORDER='com.github.tlb.splitter.TimeBasedTestSplitterCriteria:com.github.tlb.splitter.CountBasedTestSplitterCriteria' 
-export TLB_CRITERIA='com.github.tlb.splitter.DefaultingTestSplitterCriteria' 
-export TOTAL_PARTITIONS=2
-export TLB_URL='http://localhost:7019' 
-export TALK_TO_SERVICE='com.github.tlb.service.TalkToTlbServer' 
-export JOB_VERSION=1.2
-export TLB_JOB_NAME='sample-ant' 
-ant test.balanced -v
