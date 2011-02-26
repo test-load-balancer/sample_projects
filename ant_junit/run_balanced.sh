a76a14c7b1add0eeeb8909deae1cc34cd774e63e
Janmejay Singh <singh.janmejay@gmail.com>
2011-02-26 16:33:08 +0530

    issue#56 - fixing ant_junit build to play nice with the new package names

--- ant_junit/run_balanced.sh
+++ ant_junit/run_balanced.sh
@@ -2,8 +2,8 @@
 
 ant init
 
-export TLB_JAR=`ls lib/tlb-standalone/tlb-all*.jar`
-export TLB_JOB_NAME='ant-junit' 
+export TLB_JAR=`ls lib/tlb-standalone/tlb-server*.jar`
+export TLB_JOB_NAME='ant-junit'
 export TLB_TOTAL_PARTITIONS=2
 export TEST_TASK='ant test.balanced'
 
