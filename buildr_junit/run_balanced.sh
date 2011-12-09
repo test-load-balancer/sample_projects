8cbc138c9d8d790585e52b74144c1a113d4d4f78
Janmejay Singh <singh.janmejay@gmail.com>
2011-12-09 06:52:37 +0530

    issue#21 - tested on builder, fixed an edge-case bug. This finishes correctness-check, only documentation remains.

--- buildr_junit/run_balanced.sh
+++ buildr_junit/run_balanced.sh
@@ -5,5 +5,7 @@ export TLB_TOTAL_PARTITIONS=2
 
 export load_balance=true
 export TEST_TASK='buildr clean test'
+export ALL_PARTITIONS_RAN_VERIFICATION_TASK='buildr assert_all_partitions_executed'
+
 
 source ../recipe.sh
